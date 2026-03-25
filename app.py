a2092f7e5df951f2c457e1b7bff68020d27b0827
anuragotpl <anuragotpl@gmail.com>
2026-03-25 09:58:10 +0530

    Update app.py
    changes 1

--- app.py
+++ app.py
@@ -272,6 +272,7 @@ def chat():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
     with app.app_context():
         db.create_all()
 
